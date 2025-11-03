0d6c364ec3061bb4bf6be9ab135473612fade53f
treee27 <darshandineshsalunke@gmail.com>
2025-11-03 19:23:52 +0530

    initial deploy setup

--- Backend/server.js
+++ Backend/server.js
@@ -1,11 +1,10 @@
 require('dotenv').config();
 const app=require('./src/app');
 
-const PORT=3000;
+
 
 app.get('/',(req,res)=>{{
     res.send('hello darshan salunke');
 }})
-app.listen(3000,()=>{
-    console.log(`server is live on port ${PORT}`);
-})
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
