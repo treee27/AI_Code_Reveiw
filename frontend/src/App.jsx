9b86df9a5e3827891f69a55d4ef2fce911119ff1
treee27 <darshandineshsalunke@gmail.com>
2025-11-03 19:40:19 +0530

    build: frontend ready for Render deploy

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -21,7 +21,7 @@ function App() {
   }, [])
 
   async function reviewCode() {
-    const response = await axios.post('http://localhost:3000/ai/get-review', { code })
+    const response = await axios.post('https://ai-code-reveiw-1.onrender.com', { code })
     setReview(response.data)
     //console.log(response.data);
   }
